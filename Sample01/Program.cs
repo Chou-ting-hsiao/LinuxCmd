e143bfa5d3a4395beb44ebe468e21ee63ee51afb
Ting-Hsiao,Chou <kk156868@gmail.com>
2018-11-18 17:41:08 +0800

    加入專案檔案。

---
+++ Sample01/Program.cs
@@ -0,0 +1,17 @@
+﻿using Sample01.Utility;
+using System;
+
+namespace Sample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cmd = " ls -l ".Bash();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(cmd);
+
+        }
+    }
+}
